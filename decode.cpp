f4d00b899ef97f1d700405edbb3724fd13199766
satyahemanth-2002 <92287807+satyahemanth-2002@users.noreply.github.com>
2021-11-15 10:36:58 +0530

    Add files via upload

---
+++ decode.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{ 
+    int i,j,k;
+	char c ;
+	
+	printf("INPUT:\n");
+	
+	scanf("%d%d%d",&i,&j,&k);
+	
+	printf("OUTPUT:\n");
+	
+	printf("%c%c%c",i+95,j+95,k+95);
+	
+}
